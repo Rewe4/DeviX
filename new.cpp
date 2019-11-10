7e0239933d8f09fa23ed28a3eae09f00753769cf
Rewe4 <57597696+Rewe4@users.noreply.github.com>
2019-11-10 21:18:07 +0330

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a = 2;
+  cout << "this is a real number " << a;
+  a = 4;
+  cout << "\nthis is a fake number " << a;
+
+  return 0;
+}
