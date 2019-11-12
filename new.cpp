8f39d8c61ded4eafe2df28a84dcf0a499c41a039
Rewe4 <amirhossein22882242@gmail.com>
2019-11-12 23:05:45 +0330

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -2,10 +2,49 @@
 using namespace std;
 
 int main(){
-  int a = 2;
-  cout << "this is a real number " << a;
-  a = 4;
-  cout << "\nthis is a fake number " << a;
+  float num1{0}, num2{0};
+  char ans{'y'}, opt{'0'};
+  bool b{1};
+
+  cout << "V1.1\nWelcome To Calculator App\n\n ";
+  while(ans == 'y' || ans == 'Y'){
+
+  cout << "Enter Your First Number\t";
+    cin >> num1;
+    while(b){
+      cout << "\nType Your opt(+,-,*,/,=)\t";
+        cin >> opt;
+      if(opt != '='){
+      cout << "\nTypa a Number\t";
+        cin >> num2;
+
+        if(opt == '+'){
+          num1 += num2;
+        }
+        else if(opt == '-'){
+          num1 -= num2;
+        }
+        else if(opt == '*'){
+          num1 *= num2;
+        }
+        else if(opt == '/'){
+          num1 /= num2;
+        }
+        else {
+          cout << "\n\n\aError\nCalculator Restarting...";
+          ans = 'y';
+        }
+      }
+      else{
+        cout << "\nYour Answer =\t" << num1;
+        b = false;
+        cout << "\n\nRestart Again?(y/n)";
+          cin >> ans;
+        cout << "\n\n";
+      }
+    }
+  }
+  cout << "\nThank You for using ;)...";
 
   return 0;
 }
